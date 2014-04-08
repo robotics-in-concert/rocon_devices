6de0791caefc6254a92a43b6e164c144b7134d5f
DongWook Lee <dwlee@yujinrobot.com>
2014-04-08 13:23:48 +0900

    add the gitignore and message chagne and test

--- scripts/random_colors.py
+++ scripts/random_colors.py
@@ -19,6 +19,7 @@ light.xy = [random.random(),random.random()]
 """
 import random
 import rospy
+from hue_controller.msg import HueState
 from hue_controller.msg import Hue
 from hue_controller.msg import HueArray
 
@@ -27,23 +28,63 @@ class RandomColor():
 	def __init__(self):
 		print "Random Hue"
 		rospy.init_node("random_color")
-		self.hue_publisher = rospy.Publisher("/set_hue_color_xy", Hue)
+		self.hue_color_on_publisher = rospy.Publisher("/set_hue_color_on", Hue)
+		self.hue_color_xy_publisher = rospy.Publisher("/set_hue_color_xy", Hue)
+		self.hue_color_hsv_publisher = rospy.Publisher("/set_hue_color_hsv", Hue)
+		self.hue_color_ct_publisher = rospy.Publisher("/set_hue_color_ct", Hue)
+		self.hue_color_mode_publisher = rospy.Publisher("/set_hue_color_mode", Hue)
 		rospy.Subscriber("/hue_list", HueArray, self.set_valid_hues)
 		self.hue_list = []
 
 	def set_valid_hues(self, data):
 		self.hue_list = data.hue_list
-		print self.hue_list
 		pass
 	
 	def spin(self):
 		while not rospy.is_shutdown():
 			for hue in self.hue_list:
-				hue.state.x = random.random()
-				hue.state.y = random.random()
-				print "Set the color: [%f, %f]"%(hue.state.y, hue.state.y)
-				self.hue_publisher.publish(hue)		
-			rospy.sleep(0.5)
+				on = random.randint(0,1)
+				if on:
+					hue.state.on = True
+				else:
+					hue.state.on = False
+				print "Set the color on: [%s]"% str(hue.state.on)
+				self.hue_color_on_publisher.publish(hue)
+				
+				"""
+				hue.state.xy = [random.random(), random.random()]
+				print "Set the color xy: [%f, %f]"%(hue.state.xy[0], hue.state.xy[1])
+				self.hue_color_xy_publisher.publish(hue)
+				"""
+
+				"""
+				hue.state.hue = random.randint(0,65535)
+				hue.state.sat = random.randint(0,255)
+				hue.state.bri = random.randint(0,255)
+				print "Set the color hsv: [%d, %d, %d]"%(hue.state.hue, hue.state.sat, hue.state.bri)
+				self.hue_color_hsv_publisher.publish(hue)
+				"""
+				
+				"""
+				hue.state.ct = random.randint(153,500)
+				print "Set the color ct: [%d]"%(hue.state.ct)
+				self.hue_color_ct_publisher.publish(hue)
+				"""
+				"""
+				mode = random.randint(0,2)
+				if mode == 0:
+					hue.state.mode = HueState().NONE
+					print "Set the color mode: [NONE]"
+				elif mode == 1:
+					hue.state.mode = HueState().LSELECT
+					print "Set the color mode: [LSELECT]"
+				elif mode == 2:
+					hue.state.mode = HueState().COLOR_LOOP
+					print "Set the color mode: [COLOR_LOOP]"
+				
+				self.hue_color_mode_publisher.publish(hue)
+				"""
+			rospy.sleep(3.0)
 
 if __name__ == '__main__':
 	random_color = RandomColor()
--- scripts/roshue.py
+++ scripts/roshue.py
@@ -3,10 +3,11 @@
 import random
 import thread
 import time
+
 #ros
 import rospy
 from std_msgs.msg import Empty
-from hue_controller.msg import RequestHue
+from hue_controller.msg import HueState
 from hue_controller.msg import Hue
 from hue_controller.msg import HueArray
 
@@ -25,11 +26,11 @@ class RosHue():
 
         rospy.init_node(self.name)
         self.hue_list_publisher = rospy.Publisher("/hue_list", HueArray, latch=False)
+        rospy.Subscriber('/set_hue_color_on', Hue, self.set_hue_color_on)
         rospy.Subscriber('/set_hue_color_xy', Hue, self.set_hue_color_xy)
-        rospy.Subscriber('/set_hue_color_hsv', Hue, self.set_hue_color_xy)
-        rospy.Subscriber('/set_hue_color_ct', Hue, self.set_hue_color_xy)
+        rospy.Subscriber('/set_hue_color_hsv', Hue, self.set_hue_color_hsv)
+        rospy.Subscriber('/set_hue_color_ct', Hue, self.set_hue_color_ct)
         rospy.Subscriber('/set_hue_color_mode', Hue, self.set_hue_color_mode)
-
         thread.start_new_thread(self.hue_checker())
 
     def hue_checker(self):
@@ -43,63 +44,53 @@ class RosHue():
                     hue.light_id = light.light_id
                     hue.name = light.name
                     hue.state.on = light.on
-                    hue.state.x = light.xy[0]
-                    hue.state.y = light.xy[1]
+                    hue.state.xy = light.xy
                     hue.state.hue = light._hue or 0
                     hue.state.sat = light._saturation or 0
                     hue.state.bri = light._brightness or 0
-                    hue.state.mode = hue.state.NONE or light._effect or light._alert 
+                    hue.state.mode = hue.state.NONE or light._effect or light._alert
                     hue.state.transitiontime = light.transitiontime or 0
                     hues.hue_list.append(hue)
-                    print hues
                     self.hue_list_publisher.publish(hues)
 
+    def set_hue_color_on(self, data):
+        state = {}
+        state["on"] = data.state.on
+        self.bridge.set_light([data.light_id], state)
+
     def set_hue_color_xy(self, data):
-        lights = self.bridge.get_light_objects()
-        for light in lights:
-            if data.light_id == light.light_id:
-                x = data.state.x
-                y = data.state.y
-                light.xy = [x, y]
-                print "change the xy [%f, %f]" % (x, y)
-    
+        state = {}
+        state["xy"] = data.state.xy
+        self.bridge.set_light([data.light_id], state)
+
+    def set_hue_color_hsv(self, data):
+        state = {}
+        state["hue"] = data.state.hue
+        state["bri"] = data.state.bri
+        state["sat"] = data.state.sat
+        self.bridge.set_light([data.light_id], state)
+
+    def set_hue_color_ct(self, data):
+        state = {}
+        state["ct"] = data.state.ct
+        self.bridge.set_light([data.light_id], state)
+
     def set_hue_color_mode(self, data):
-        lights = self.bridge.get_light_objects()
-        for light in lights:
-            x = data.x
-            y = data.y
-            light.xy = [x, y]
-            print "change the xy [%f, %f]" % (x, y)
-        
-    def req_on_lights(self, data):
-        lights = self.bridge.get_light_objects()
-        for light in lights:
-            light.on = True
-
-    def req_off_lights(self, data):
-        lights = self.bridge.get_light_objects()
-        for light in lights:
-            light.on = False
-
-    def req_random_color(self, data):
-        lights = self.bridge.get_light_objects()
-
-        for light in lights:
-            light.brightness = 254
-            x = random.random()
-            y = random.random()
-            print "change the [%f, %f]" % (x, y)
-
-    def req_crazy_random_color(self, data):
-        for k in range(10):
-            lights = self.bridge.get_light_objects()
-            for light in lights:
-                if light.light_id == 2:
-                    light.brightness = 254
-                    x = random.random()
-                    y = random.random()
-                    print "change the [%f, %f]" % (x, y)
-                    light.xy = [x, y]
+        state = {}
+        if data.state.mode == HueState().NONE:
+            state["alert"] = data.state.mode
+            state["effect"] = data.state.mode
+        elif data.state.mode == HueState().COLOR_LOOP:
+            state["alert"] = data.state.NONE
+            state["effect"] = data.state.mode
+        elif data.state.mode == HueState().SELECT or data.state.mode == HueState().LSELECT:
+            state["alert"] = data.state.mode
+            state["effect"] = data.state.NONE
+        else:
+            state["alert"] = data.state.mode
+            state["effect"] = data.state.mode
+        print state
+        self.bridge.set_light([data.light_id], state)
 
     def spin(self):
         while not rospy.is_shutdown():
