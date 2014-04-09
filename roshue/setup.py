b0a65fedcfd38c65aaeb6baac05d3784b4036023
DongWook Lee <dwlee@yujinrobot.com>
2014-04-09 16:39:28 +0900

    clean up CMakeList and setup.py

--- roshue/setup.py
+++ roshue/setup.py
@@ -6,6 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['roshue'],
     package_dir={'': 'src'},
-    scripts=['scripts'],
+    scripts=['scripts/roshue_bridge.py'],
 )
 setup(**d)
