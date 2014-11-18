c4e1b6f20ab21d15e785d4bab8b16cd51dd8b31d
dwlee <dwlee@yujinrobot.com>
2014-11-18 17:38:41 +0900

    move nb package

--- rocon_ninja_block/src/rocon_ninja_block/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_devices/license/LICENSE
-#
-#################################################################################
-from .ninja_block_bridge import *
-from .exceptions import *
