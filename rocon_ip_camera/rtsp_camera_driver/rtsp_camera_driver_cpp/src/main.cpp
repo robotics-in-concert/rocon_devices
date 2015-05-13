bcaa4e382bb8be36f17a337836626274ea283f6c
dwlee <dwlee@yujinrobot.com>
2015-05-13 17:51:01 +0900

    update publisher for camera info

--- rocon_ip_camera/rtsp_camera_driver/rtsp_camera_driver_cpp/src/main.cpp
+++ rocon_ip_camera/rtsp_camera_driver/rtsp_camera_driver_cpp/src/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
  
 int main (int argc, char *argv[]) 
 { 
-     ros::init(argc, argv, "rtsp_camera_driver");
+     ros::init(argc, argv, "rtsp_camera");
      
      VideoCapture vcap; 
      Mat image;
@@ -20,13 +20,23 @@ int main (int argc, char *argv[])
      ros::NodeHandle nh;
      image_transport::ImageTransport img_trs(nh);
      image_transport::Publisher img_pub;
-     img_pub = img_trs.advertise("rtsp_camera", 1);
+     img_pub = img_trs.advertise("~image", 1);
+     ros::Publisher chatter_pub = nh.advertise<sensor_msgs::CameraInfo>("~camera_info", 1);
 
      cv_bridge::CvImage cv_img;
-     string videoStreamAddress = 
-          "rtsp://yujin:akqjqtk@192.168.10.141:88/videoSub";  
-     ROS_INFO("%s",videoStreamAddress.c_str());
-     if (!vcap.open(videoStreamAddress)) { 
+     sensor_msgs::CameraInfo camera_info
+     
+     string user = "yujin";
+     string password = "akqjqtk";
+     string camera_address = "192.168.10.101:88";
+     string video_stream_api = "/videoSub";
+
+     string video_stream_address = 
+          "rtsp://" + user + ":" + password + "@" + camera_address + video_stream_api;
+
+
+     ROS_INFO("%s",video_stream_address.c_str());
+     if (!vcap.open(video_stream_address)) { 
           ROS_ERROR("%s","Error opening video stream or file");
           return -1; 
      }
@@ -39,6 +49,12 @@ int main (int argc, char *argv[])
           cv_img.encoding = sensor_msgs::image_encodings::BGR8;
           cv_img.image = image;
           img_pub.publish(cv_img.toImageMsg());
+
+          ROS_INFO("image.rows [%d]",image.rows);
+          ROS_INFO("image.cols [%d]",image.rows);
+          ROS_INFO("image.step [%d]",image.step);
+          ROS_INFO("image.steps [%d]",image.steps);
+
 
           imshow("Output Window", image); 
           waitKey(1); 
