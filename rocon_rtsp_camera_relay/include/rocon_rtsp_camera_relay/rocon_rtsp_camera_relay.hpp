3d59fb31287f8b6b55b1645a569e5802c4fb37e3
Jihoon Lee <jihoonlee.in@gmail.com>
2015-05-14 15:57:56 +0900

    rtsp camera relay package ready

---
+++ rocon_rtsp_camera_relay/include/rocon_rtsp_camera_relay/rocon_rtsp_camera_relay.hpp
@@ -0,0 +1,44 @@
+/*
+ License: BSD
+   https://raw.github.com/robotics-in-concert/rocon_devices/license/LICENSE
+*/
+
+#ifndef ROCON_RTSP_CAMERA_RELAY
+#define ROCON_RTSP_CAMERA_RELAY
+
+#include<ros/ros.h>
+#include<opencv2/opencv.hpp>
+#include<cv_bridge/cv_bridge.h>
+
+#include<std_msgs/String.h>
+#include<image_transport/image_transport.h>
+#include<sensor_msgs/image_encodings.h>
+
+namespace rocon {
+
+class RoconRtspCameraRelay {
+  public:
+    RoconRtspCameraRelay(ros::NodeHandle& n);
+    ~RoconRtspCameraRelay();
+
+    bool init(const std::string video_stream_url,const std::string user,const std::string password);
+    bool reset(const std::string video_stream_url,const std::string user,const std::string password);
+
+    void spin();
+  
+  protected:
+    void convertCvToRosImg(const cv::Mat& mat, sensor_msgs::Image& ros_img, sensor_msgs::CameraInfo& ci);
+
+  private:
+    cv::VideoCapture                vcap_;
+    std::string                     video_stream_address_;
+    std::string                     status_;
+
+    image_transport::Publisher pub_video_;
+    ros::Publisher pub_camera_info_;
+    ros::Publisher pub_status_;
+    ros::NodeHandle nh_;
+};
+}
+
+#endif
--- rocon_rtsp_camera_relay/src/main.cpp
+++ rocon_rtsp_camera_relay/src/main.cpp
@@ -1,65 +1,32 @@
+/*
+ License: BSD
+   https://raw.github.com/robotics-in-concert/rocon_devices/license/LICENSE
+*/
+
 #include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
+#include <rocon_rtsp_camera_relay/rocon_rtsp_camera_relay.hpp>
 
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
-#include <iostream>
- 
-using namespace cv;
-using namespace std;
- 
-int main (int argc, char *argv[]) 
-{ 
-     ros::init(argc, argv, "rtsp_camera");
-     
-     VideoCapture vcap; 
-     Mat image;
+int main (int argc, char** argv) 
+{
+  ros::init(argc, argv, "rtsp_camera_relay");
+  ros::NodeHandle pnh("~");
+  std::string video_stream_url, user, password;
 
-     ros::NodeHandle nh;
-     image_transport::ImageTransport img_trs(nh);
-     image_transport::Publisher img_pub;
-     img_pub = img_trs.advertise("~image", 1);
-     ros::Publisher chatter_pub = nh.advertise<sensor_msgs::CameraInfo>("~camera_info", 1);
+  pnh.getParam("video_stream_url", video_stream_url);
+  pnh.getParam("user", user);
+  pnh.getParam("password", password);
 
-     cv_bridge::CvImage cv_img;
-     sensor_msgs::CameraInfo camera_info
-     
-     string user = "yujin";
-     string password = "akqjqtk";
-     string camera_address = "192.168.10.101:88";
-     string video_stream_api = "/videoSub";
+  rocon::RoconRtspCameraRelay rtsp(pnh);
+  ROS_INFO("Rtsp Camera : Initialising..");
+  if(!rtsp.init(video_stream_url, user, password))
+  {
+    ROS_ERROR("Rtsp Camera : Failed to initialise stream");
+    return -1;
+  }
 
-     string video_stream_address = 
-          "rtsp://" + user + ":" + password + "@" + camera_address + video_stream_api;
+  ROS_INFO("Rtsp Camera : Initialised");
+  rtsp.spin();
+  ROS_INFO("Rtsp Camera : Bye Bye");
 
-
-     ROS_INFO("%s",video_stream_address.c_str());
-     if (!vcap.open(video_stream_address)) { 
-          ROS_ERROR("%s","Error opening video stream or file");
-          return -1; 
-     }
- 
-     while(ros::ok()) { 
-          if(!vcap.read(image)) { 
-               ROS_WARN("%s","No frame");
-               waitKey();
-          }
-          cv_img.encoding = sensor_msgs::image_encodings::BGR8;
-          cv_img.image = image;
-          img_pub.publish(cv_img.toImageMsg());
-
-          ROS_INFO("image.rows [%d]",image.rows);
-          ROS_INFO("image.cols [%d]",image.rows);
-          ROS_INFO("image.step [%d]",image.step);
-          ROS_INFO("image.steps [%d]",image.steps);
-
-
-          imshow("Output Window", image); 
-          waitKey(1); 
-     } 
-     vcap.release();
-     return 0;
+  return 0;
 }
-
