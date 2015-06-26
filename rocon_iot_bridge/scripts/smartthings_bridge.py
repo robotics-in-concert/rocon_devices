5f07aa6988aa843eebe0d1a35ed3bfeca182405f
Jihoon Lee <jihoonlee.in@gmail.com>
2015-06-26 17:50:31 +0900

    smartthings bridge working

--- rocon_iot_bridge/scripts/smartthings_bridge.py
+++ rocon_iot_bridge/scripts/smartthings_bridge.py
@@ -5,6 +5,8 @@
 #
 #################################################################################
 
+import json
+import requests
 import rospy
 from rocon_iot_bridge import RoconIOTBridge, Connector
 
@@ -17,9 +19,9 @@ class SmartThingsConnector(Connector):
 
     def __init__(self):
         self._configuration_file = rospy.get_param('~target_configuration_file')
-        self._load_configuration()
+        self._load_configuration(self._configuration_file)
 
-    def _load_configuration(self):
+    def _load_configuration(self, filename):
         with open(filename) as f:
             config = json.load(f) 
         self._config = config
@@ -29,21 +31,21 @@ class SmartThingsConnector(Connector):
 
         if config:
             return self.request_configuration_update(config)
-        else
+        else:
             return None 
 
     def call_get_device_list(self):
         request_url = "%s/%s"%(url, self._API['GET_DEVICE_LIST'])
         params = {}
         header =  {
-          "Authorization": "Bearer %s" % access_token,
+          "Authorization": "Bearer %s" % self._config['access_token'],
         }
         resp = requests.get(url=request_url, params=params, headers=header)
         print(resp.json())
 
     def request_configuration_update(self, config):
         request_url = "%s/%s"%(self._endpoint_url, self._API['UPDATE_CONFIG'])
-        header = { "Authorization": "Bearer %s" % access_token,}
+        header = { "Authorization": "Bearer %s" % self._config['access_token'],}
         resp = requests.put(url=request_url, params=config, headers=header)
         
         # Return true or false
@@ -60,7 +62,7 @@ class SmartThingsConnector(Connector):
 
         endpoints_response = requests.get(url=endpoints_url, params=endpoints_paramd)
         end_url = endpoints_response.json()[0]['url']
-        endpoint_url = 'http://%s%s'%(api_location, end_url)
+        endpoint_url = 'http://%s%s'%(self._config['api_location'], end_url)
 
         return endpoint_url
 
@@ -68,11 +70,13 @@ class SmartThingsConnector(Connector):
 if __name__ == '__main__':
     rospy.init_node('smartthings_bridge')
     
-    address = rospy.get_param('~address')
-    port    = rospy.get_param('~port')
+    local_address = rospy.get_param('~local_address')
+    local_port    = rospy.get_param('~local_port')
+    global_address = rospy.get_param('~global_address')
+    global_port    = rospy.get_param('~global_port')
 
     connector = SmartThingsConnector()
-    bridge = RoconIOTBridge(address, port, connector)
+    bridge = RoconIOTBridge(local_address, local_port, global_address, global_port, connector)
 
     bridge.loginfo("Initilialised")
     bridge.spin()
--- rocon_iot_bridge/src/rocon_iot_bridge/rocon_iot_bridge.py
+++ rocon_iot_bridge/src/rocon_iot_bridge/rocon_iot_bridge.py
@@ -5,7 +5,8 @@
 #
 
 import rospy
-import flask
+import requests
+import threading
 import rocon_device_msgs.srv as rocon_device_srvs
 import rocon_device_msgs.msg as rocon_device_msgs
 
@@ -13,38 +14,76 @@ from flask import Flask
 from flask import request
 
 class RoconIOTBridge(object):
-    def __init__(self, address, port, connector):
+    def __init__(self, local_address, local_port, global_address, global_port, connector):
         self._app = Flask(rospy.get_name())
-        self._address = address
-        self._port = port
+        self._local_address = local_address
+        self._local_port = local_port
+        self._global_address = global_address
+        self._global_port = global_port
         self._connector = connector
+
 
     def _init_flask(self):
         self._app.add_url_rule('/index', view_func=self._index, methods=['GET'])
         self._app.add_url_rule('/devices', view_func=self._received_devices_event, methods=['POST'])
+        self._app.add_url_rule(rule='/shutdown', view_func=self._shutdown_flask)
+
+        self._server_thread = threading.Thread(target=self._app.run, kwargs={'host': self._local_address, 'port': self._local_port, 'debug': False}) 
 
     def _init_ros_api(self):
-        self._srv_get_device_list = rospy.Service('get_device_list', rocon_device_msgs.GetDeviceList, self._process_get_device_list)
+        self._srv_get_device_list = rospy.Service('get_device_list', rocon_device_srvs.GetDeviceList, self._process_get_device_list)
         self._pub_device_event = rospy.Publisher('devices', rocon_device_msgs.Devices, queue_size=2)
 
     def spin(self):
         self._init_flask()
         self._init_ros_api()
 
-        subscription_uri = "%s:%s/devices"
-        self.loginfo(subscription_uri)
-        resp = self._connector().init({uri:subscription_uri})
+        subscription_uri = "%s:%s/devices"%(self._global_address, self._global_port)
+        self.loginfo("initialising with %s"%subscription_uri)
+        resp = self._connector.init({"address": self._global_address, "port": self._global_port, "api":"devices"})
         if resp:
             self.loginfo(str(resp))
-        rospy.on_shutdown(self._shutdown_flask)
-        self._app.run(host=self._address, port=self._port, debug=False)
 
-    def _shutdown_flask(self):
-        self.loginfo('request shutdown')
+        self.loginfo("Starting bridge server...")
+        self._server_thread.start()
+        self.loginfo("Started...")
+
+        while not rospy.is_shutdown():
+            rospy.sleep(1.0)
+
+        result, e = self._shutdown_server()
+        self.loginfo(e)
+        self._server_thread.join(1)
+
+    def _received_devices_event(self):
+        j = request.json
+        self.loginfo("Received : %s"%str(j))
+
+        return "Hola"
+
+    def _process_get_device_list(self, req):
+        return rocon_device_srvs.GetDeviceListResponse()
+
+    def _index(self):
+        self.loginfo("index!")
+        return "Index!"
+
+    def _shutdown_flask(self, methods=['GET']):
+        self.loginfo('shutdown requested')
         func = request.environ.get('werkzeug.server.shutdown')
         if func is None:
             raise RuntimeError('Not running with the Werkzeug Server')
         func()
+
+    def _shutdown_server(self):
+        try:
+            address = 'http://' + self._local_address + ':' + str(self._local_port) + '/shutdown'
+            resp = requests.get(address)
+        except Exception, e:
+            return (False, e)
+        else:
+            return (True, 'server shutdown success!!')
+
 
     def loginfo(self, msg):
         rospy.loginfo("%s : %s"%(rospy.get_name(), msg))
